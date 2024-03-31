143c66fc91f619a74ad510fd9da353f40e10e654
dev4449 <mohsen.salahshoor130@gmail.com>
2024-03-31 15:40:55 +0330

    Added Astro's official sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mohsenmajd.dev",
   i18n: {
     defaultLocale: "en-US",
-    locales: ["en-US", "fa-IR"]
+    locales: ["en-US", "fa-IR"],
   },
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), sitemap()],
 });
